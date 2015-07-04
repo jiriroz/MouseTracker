65dc9f42c8b05c636a6695e6968032e0bbfa26fb
jirkaroz <jiri.roznovjak@gmail.com>
2015-07-03 20:19:44 -0400

    data storage fnally

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-/*var assert = require('assert');
+
+/*
 var MongoClient = require('mongodb').MongoClient;
-
 var dburl = 'mongodb://localhost:27017/database';
-
 var DB;
 var collection;
-
 MongoClient.connect(dburl, function(err, db) {
     if(err) {
         return console.log(err);
     } else {
         console.log("connected");
         DB = db;
-        collection = db.collection("test");
+        collection = db.collection("data", function (err, coll) {});
+        
     }
 });*/
 
-fs.writeFile("log.txt", "", function(err) {
-    if (err) {
-        return console.log(err);
-    } else {
-        console.log("Log created.");
-    }
-});
-//handle the case when data is received before the file is created
-
+createLog();
 
 http.createServer(function(request, response){
     var parsed = url.parse(request.url);
@@ -43,9 +34,9 @@ http.createServer(function(request, response){
             });
         }
         if (parsed.pathname == "//getid") {
-            var id = Math.floor(Math.random() * 1000); //for now random
+            var ID = Math.floor(Math.random() * 1000); //for now random
             response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.end(id.toString(), "utf-8");
+            response.end(ID.toString(), "utf-8");
         }
     }
     if (request.method == 'POST') {
@@ -57,26 +48,52 @@ http.createServer(function(request, response){
                 request.connection.destroy();
         });
         request.on('end', function () {
-            var jsonData = JSON.parse(body);
-            var moves = jsonData["moves"];
-            var save = "";
-            for (var i = 0; i < moves.length; i++) {
-                var line = moves[i][0] + " " + moves[i][1] + " " +
-                           moves[i][2] + "\n";
-                save += line;
-            }
-            console.log(save);
-            fs.appendFile("log.txt", save, function(err) {
-                if (err) {
-                    return console.log(err);
-                } else {
-                    //console.log("Data written.");
-                }
-            });
+            var data = JSON.parse(body);
+            saveData(data);
         });
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.end("");
     }
-
 }).listen(8001);
 console.log("server initialized");
+
+function createLog() {
+    fs.writeFile("log.csv", "x,y,t,type,id\n", function(err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Log created.");
+        }
+    });
+}
+
+function saveData(data) {
+    var id = data["id"];
+    var body = "";
+    for (var i = 0; i < data["clicks"].length; i++) {
+        var line = data["clicks"][i][0] + "," + data["clicks"][i][1] + "," +
+                   data["clicks"][i][2] + ",c," + id + "\n";
+        body += line;
+    }
+    for (var i = 0; i < data["moves"].length; i++) {
+        var line = data["moves"][i][0] + "," + data["moves"][i][1] + "," +
+                   data["moves"][i][2] + ",m," + id + "\n";
+        body += line;
+    }
+    for (var i = 0; i < data["scrolls"].length; i++) {
+        var line = "-,-," + data["scrolls"][i][0] + ",s," + id + "\n";
+        body += line;
+    }
+    for (var i = 0; i < data["hovers"].length; i++) {
+        var line = data["hovers"][i][0] + ",-," + data["hovers"][i][1] + 
+                   ",h," + id + "\n";
+        body += line;
+    }
+    fs.appendFile("log.csv", body, function(err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Appended.");
+        }
+    });
+}
